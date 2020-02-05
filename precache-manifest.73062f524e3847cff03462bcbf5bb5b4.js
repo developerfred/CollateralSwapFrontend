862eec889a1dd2481e1d4aa80dd789a2fd7c530a
David Truong <david@truong.vc>
2020-02-05 22:57:56 +0100

    Updates

---
+++ precache-manifest.73062f524e3847cff03462bcbf5bb5b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "190c200d485b3be84f11ea579934cea3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09e3d06134084d261d59",
+    "url": "/static/js/2.07634b78.chunk.js"
+  },
+  {
+    "revision": "6d6c0d1371e570614ef23e12cbfed171",
+    "url": "/static/js/2.07634b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09377f65974f6ec27529",
+    "url": "/static/js/main.f2e0a39a.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
